e5cae9a91595f1b976fda36978524b5fe05b7c1e
SShipon <nxshipon@gmail.com>
2024-02-16 21:20:01 +0600

    error solved

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Sidebar from './layout/Sidebar/Sidebar';
+import Content from './layout/Content/Content';
+
+export default function App() {
+  return (
+    <>
+    <div className='app'>
+      <Sidebar />
+      <Content />
+    </div>
+  </>
+  )
+}
