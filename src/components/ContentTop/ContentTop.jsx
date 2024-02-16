baf9aa811958b7fda0f1cf5fa2f7e7d7c25345bb
sabrina <sabrinaahmednitu@gmail.com>
2024-02-16 22:00:00 +0600

    navbar user image

--- src/components/ContentTop/ContentTop.jsx
+++ src/components/ContentTop/ContentTop.jsx
@@ -35,7 +35,7 @@ const ContentTop = () => {
               role="button"
               className="btn btn-ghost btn-circle avatar"
             >
-              <div className="w-12 rounded-full">
+              <div className="w-12 rounded ">
                 <img
                   alt="Tailwind CSS Navbar component"
                   src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
