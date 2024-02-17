eac7a02bd8e5f0d413ed7b1e40b96157dac5175b
sabrina <sabrinaahmednitu@gmail.com>
2024-02-17 14:22:07 +0600

    global right done

--- src/components/Home/Global/Global.jsx
+++ src/components/Home/Global/Global.jsx
@@ -6,6 +6,8 @@ import facebook from '../../../assets/Home/facebook.png';
 import facebookCurve from '../../../assets/Home/Vector 8.png';
 import microsoft from '../../../assets/Home/microsoft.png';
 import microsoftCurve from '../../../assets/Home/Group 3.png';
+import google from '../../../assets/Home/google.png';
+import googleCurve from '../../../assets/Home/Vector 12.png';
 
 const Global = () => {
   return (
@@ -81,28 +83,28 @@ const Global = () => {
             </div>
             <div className="box1-flex">
               <p>Uptime</p>
-              <h4>89%</h4>
+              <h4>100%</h4>
             </div>
             <div className="box1-flex">
               <p>Load Average</p>
               <h6>
-                250 ms <i class="fa-solid fa-arrow-down"></i>
+                250 ms <i class="fa-solid fa-arrow-up"></i>
               </h6>
             </div>
           </div>
           <div className="global-right-box">
             <div className="box1-flex">
               <div className="box1-part1">
-                <img src={facebook} alt="" />
-                <h3>Meta</h3>
+                <img src={google} alt="" />
+                <h3>Google</h3>
               </div>
               <div>
-                <img src={facebookCurve} alt="" />
+                <img src={googleCurve} alt="" />
               </div>
             </div>
             <div className="box1-flex">
               <p>Uptime</p>
-              <h4>89%</h4>
+              <h4>98.9%</h4>
             </div>
             <div className="box1-flex">
               <p>Load Average</p>
