38c9274d60870bd40b1d5e9185dfdb650f4f6ba3
sabrina <sabrinaahmednitu@gmail.com>
2024-02-18 23:17:50 +0600

    recent monitor top

--- src/components/Home/Recent/Recent.jsx
+++ src/components/Home/Recent/Recent.jsx
@@ -5,7 +5,16 @@ const Recent = () => {
   return (
     <div>
       <div className="recent-container">
-        <div className="recent-monitors-left"></div>
+        <div className="recent-monitors-left">
+          <div className="recent-monitors-box">
+            <div>
+              <h1>Recent Monitors</h1>
+                      </div>
+                      <div>
+                          <button>See More</button>
+                      </div>
+          </div>
+        </div>
         <div className="incidents-history-right"></div>
       </div>
     </div>
--- src/layout/Content/Content.jsx
+++ src/layout/Content/Content.jsx
@@ -5,6 +5,7 @@ import WebMonitors from "../../components/WebMonitors/WebMonitors";
 import Welcome from "../../components/Home/Welcome/Welcome";
 import Global from "../../components/Home/Global/Global";
 import Monitors from "../../components/Home/Monitors/Monitors";
+import Recent from "../../components/Home/Recent/Recent";
 
 
 const Content = () => {
@@ -16,6 +17,7 @@ const Content = () => {
       <Welcome></Welcome>
       <Global></Global>
       <Monitors></Monitors>
+      <Recent></Recent>
 
 {/* shipon */}
     
