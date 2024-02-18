2dbe0f3eceb5d4df9d86ad6bbb7a8d8ecf7f0451
sabrina <sabrinaahmednitu@gmail.com>
2024-02-18 22:44:42 +0600

    some measurement updated

---
+++ src/components/Home/Recent/Recent.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Recent.css';
+
+const Recent = () => {
+  return (
+    <div>
+      <div className="recent-container">
+        <div className="recent-monitors-left"></div>
+        <div className="incidents-history-right"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Recent;
